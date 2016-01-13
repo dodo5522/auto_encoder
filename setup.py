047c3d4c380a5f02cb639818087b4098bb8d26e5
Takashi Ando <dodo5522@gmail.com>
2016-01-13 23:11:51 +0900

    add setup.py and related files.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+
+def readme():
+    try:
+        os.system("pandoc --from=markdown --to=rst README.md -o README.rst")
+        with open("README.rst", "r") as f:
+            return f.read()
+    except:
+        return ""
+
+
+def requirements():
+    try:
+        return [pkg.strip() for pkg in open('requirements.txt', 'r').readlines()]
+    except:
+        return []
+
+
+setup(
+    name='auto-encoder',
+    version='0.0.1',
+    description='Frontend encode tool with avconv.',
+    long_description=readme(),
+    author='Takashi Ando',
+    url='',
+    packages=[
+        'auto_encoder'],
+    install_requires=requirements())
