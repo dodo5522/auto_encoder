b7f51c2c241168e2932a47db4b5fe627d5f017b8
Takashi Ando <dodo5522@gmail.com>
2016-01-16 13:04:00 +0900

    Add test case files.

---
+++ test/test_encoder.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import unittest
+
+
+class TestEncoder(unittest.TestCase):
+    """Test case for encoder function."""
+
+    @classmethod
+    def setUpClass(cls):
+        """setup class"""
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """teardown class"""
+        pass
+
+    def setUp(self):
+        """setup"""
+        pass
+
+    def tearDown(self):
+        """teardown"""
+        pass
+
+    def test_encode_(self):
+        """aaa"""
+        pass
+
+    def test_get_keywords_(self):
+        """aaa"""
+        pass
+
+    def test_get_src_dst_(self):
+        """aaa"""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
---
+++ test/test_main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import unittest
+
+
+class TestMain(unittest.TestCase):
+    """Test case for main function."""
+
+    @classmethod
+    def setUpClass(cls):
+        """setup class"""
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """teardown class"""
+        pass
+
+    def setUp(self):
+        """setup"""
+        pass
+
+    def tearDown(self):
+        """teardown"""
+        pass
+
+    def test_main_(self):
+        """aaa"""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
